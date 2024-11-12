5ee5d77b59e3c34500865f5c6714954d6625dd06
José Mario da Silva Santos <jose.santos@intranet.ciss>
2024-11-12 12:00:04 -0300

    código refatorado e implementação da tela de login

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
-const { Pool } = require('pg');
+require('dotenv').config()
+const { Pool, Client } = require('pg');
+const config = {
+    user: process.env.USER,
+    host: process.env.HOST,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+}
+const client = new Client(config)
+client.connect()
+    .then(() => console.log('Conectado'))
+    .catch((error) => console.error('Erro na conexão:', error))
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'postgres',
-    database: 'postgres',
-    port: 5432
-})
 
-module.exports = pool;
+module.exports = client;

535bc69e1731116d6e49d52e4d1c797bbec15d4d
José Mario da Silva Santos <jose.santos@intranet.ciss>
2024-11-27 12:01:32 -0300

    instalado e configurada ejs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config
+const path = require('path')
 const express = require("express")
 const app = express()
 const port = 8081
@@ -6,6 +7,9 @@ const routerUsuario = require("./routes/usuarioRoutes")
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json()); 
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get("/", function(req, res){
     res.send("Test")

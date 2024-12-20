bf46748d2f2b14f91d27a1e26ff35ee8278c014d
José Mário <josemario.g12001@gmail.com>
2024-12-19 22:07:59 -0300

    barra de pesquisa funcional

--- controllers/projetoController.js
+++ controllers/projetoController.js
@@ -3,25 +3,78 @@ const ProjetoDevs = require("../models/projeto_desenvolvedoresModels")
 const ProjetoPalavraChave = require("../models/projeto_palavra_chaveModels")
 const Usuario = require("../models/usuarioModels")
 const PalavraChave = require("../models/palavra_chaveModels")
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 exports.paginaListarProjetosAll = async (req, res) => {
     try {
-        const projetos = await Projeto.findAll({
-            attributes: ["id_projeto", "nome_projeto", "resumo_projeto", "link_externo"], 
-        });
+        const { busca } = req.query;
+        let projetos;
+
+        if (busca) {
+            // Buscar projetos que correspondam ao termo de busca
+            const projetosPorNome = await Projeto.findAll({
+                where: {
+                    [Op.or]: [
+                        { nome_projeto: { [Op.iLike]: `%${busca}%` } },
+                        { resumo_projeto: { [Op.iLike]: `%${busca}%` } }
+                    ]
+                },
+                attributes: ["id_projeto", "nome_projeto", "resumo_projeto", "link_externo"]
+            });
+
+            // Buscar projetos por palavras-chave
+            const palavrasChave = await PalavraChave.findAll({
+                where: {
+                    nome_palavra_chave: { [Op.iLike]: `%${busca}%` }
+                }
+            });
+
+            const idsPalavrasChave = palavrasChave.map(pc => pc.id_palavra_chave);
+            const projetosPorPalavraChave = await ProjetoPalavraChave.findAll({
+                where: {
+                    id_palavra_chave: idsPalavrasChave
+                }
+            });
+
+            const idsProjetosPorPalavraChave = projetosPorPalavraChave.map(pp => pp.id_projeto);
+            const projetosPorPalavras = await Projeto.findAll({
+                where: {
+                    id_projeto: idsProjetosPorPalavraChave
+                },
+                attributes: ["id_projeto", "nome_projeto", "resumo_projeto", "link_externo"]
+            });
+
+            // Combinar resultados e remover duplicatas
+            const todosIds = [...new Set([
+                ...projetosPorNome.map(p => p.id_projeto),
+                ...projetosPorPalavras.map(p => p.id_projeto)
+            ])];
+
+            projetos = await Projeto.findAll({
+                where: {
+                    id_projeto: todosIds
+                },
+                attributes: ["id_projeto", "nome_projeto", "resumo_projeto", "link_externo"]
+            });
+        } else {
+            projetos = await Projeto.findAll({
+                attributes: ["id_projeto", "nome_projeto", "resumo_projeto", "link_externo"]
+            });
+        }
 
         const idsProjetos = projetos.map(projeto => projeto.id_projeto);
 
         const projetosDevs = await ProjetoDevs.findAll({
             where: {
-                id_projeto: idsProjetos, 
-            },
+                id_projeto: idsProjetos
+            }
         });
 
         const projetosPalavrasChave = await ProjetoPalavraChave.findAll({
             where: {
-                id_projeto: idsProjetos,
-            },
+                id_projeto: idsProjetos
+            }
         });
 
         const idsUsuarios = [...new Set(projetosDevs.map(pd => pd.id_usuario))];
@@ -29,16 +82,16 @@ exports.paginaListarProjetosAll = async (req, res) => {
 
         const usuarios = await Usuario.findAll({
             where: {
-                id_usuario: idsUsuarios, 
+                id_usuario: idsUsuarios
             },
-            attributes: ["id_usuario", "nome_usuario", "email"],
+            attributes: ["id_usuario", "nome_usuario", "email"]
         });
 
         const palavrasChave = await PalavraChave.findAll({
             where: {
-                id_palavra_chave: idsPalavrasChave,
+                id_palavra_chave: idsPalavrasChave
             },
-            attributes: ["id_palavra_chave", "nome_palavra_chave"],
+            attributes: ["id_palavra_chave", "nome_palavra_chave"]
         });
 
         const usuariosMap = usuarios.reduce((map, usuario) => {
@@ -51,33 +104,30 @@ exports.paginaListarProjetosAll = async (req, res) => {
             return map;
         }, {});
 
-        const allProject = projetos.map(projeto => {
-            const usuariosRelacionados = projetosDevs
+        const projetosFormatados = projetos.map(projeto => {
+            const desenvolvedores = projetosDevs
                 .filter(pd => pd.id_projeto === projeto.id_projeto)
-                .map(pd => usuariosMap[pd.id_usuario]);
+                .map(pd => usuariosMap[pd.id_usuario])
+                .filter(Boolean);
 
-            const palavrasRelacionadas = projetosPalavrasChave
+            const palavras = projetosPalavrasChave
                 .filter(pp => pp.id_projeto === projeto.id_projeto)
-                .map(pp => palavrasChaveMap[pp.id_palavra_chave]);
+                .map(pp => palavrasChaveMap[pp.id_palavra_chave])
+                .filter(Boolean);
 
             return {
-                ...projeto.dataValues,
-                Usuarios: usuariosRelacionados,
-                PalavrasChave: palavrasRelacionadas, 
+                ...projeto.get(),
+                desenvolvedores,
+                palavrasChave: palavras
             };
         });
 
-        res.render("listarProjetos", {
-            title: "Projetos",
-            allProject,
-        });
+        res.render('listarProjetos', { projetos: projetosFormatados, termoBusca: busca || '' });
     } catch (error) {
-        return res
-            .status(500)
-            .json({ error: "Não foi possível carregar a página", details: error.message });
+        console.error('Erro ao listar projetos:', error);
+        res.status(500).json({ error: 'Erro ao listar projetos', details: error.message });
     }
 };
-
 
 
 exports.paginaCriarProjeto = async ( req, res ) => {
